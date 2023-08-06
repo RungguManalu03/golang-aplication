e6e023ef3a82c7b23593f2c928d7196c3197a7f1
RungguManalu <runggumanalu8@gmail.com>
2023-08-06 22:37:29 +0700

    email checker

--- handler/user.go
+++ handler/user.go
@@ -73,3 +73,41 @@ func (h *userHandler) Login(c *gin.Context) {
 
 	c.JSON(http.StatusOK, response)
 }
+
+func (h *userHandler) CheckEmailAvailability(c *gin.Context) {
+	var input user.CheckEmailInput
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+
+		response := helper.APIResponse("Email checking failed", http.StatusUnprocessableEntity, "error", errorMessage) 
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	isEmailAvailable, err := h.userService.IsEmailAvailable(input)
+	if err != nil {
+		errorMessage := gin.H{"errors": "Server error"}
+
+		response := helper.APIResponse("Email checking Failed", http.StatusUnprocessableEntity, "error", errorMessage) 
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	data := gin.H{
+		"is_avaliable": isEmailAvailable,
+	}
+
+	var metaMessage string
+
+	metaMessage = "Email has been registered"
+
+	if isEmailAvailable {
+		metaMessage = "Email is available"
+	}
+
+	response := helper.APIResponse(metaMessage, http.StatusOK, "error", data) 
+	c.JSON(http.StatusUnprocessableEntity, response)
+}
